2b3721e07fb757df05ce2e12868aa45049729fb3
queil <queil@users.noreply.github.com>
2022-09-02 10:41:59 +0100

    Moved code from the ring tool repo

---
+++ src/ATech.Ring.Protocol/IReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace ATech.Ring.Protocol
+{
+    public interface IReceiver<T>
+    {
+        bool TryDequeue(out T item);
+        Task<T> WaitForNextAsync(CancellationToken token);
+    }
+}
